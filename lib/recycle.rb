e35238b66797f8966c33750d50e0d872a5183b42
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-06-23 21:54:34 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -1,8 +1,8 @@
 ###### Code Your Solutions Here!!!
 
 #Your code should work with a hash that looks like the one below (in comments).
-#The specs (which call your tests) are using these exact hashes 
-# We haven't hard coded the hashes in the tests, 
+#The specs (which call your tests) are using these exact hashes
+# We haven't hard coded the hashes in the tests,
 #they're dynamically brought in from the NYC Open Data API
 
 # first_recycle_bin =  {
@@ -17,12 +17,15 @@
 
 #1. Define a method `address`, that takes a hash as an argument.
 #This method should return the value of the address key in the hash.
+def address(hash)
+  
+end
 
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
 
 #3. Define a method called `all_keys` that takes a hash as an argument.
-#This method should print out each key in the hash. 
+#This method should print out each key in the hash.
 #You should use the `each_key` method to iterate over your hash and print out each key.
 
 #4. Define a method called `all_data` that takes a hash as an argument.
